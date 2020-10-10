e6c8060c955e34a54112a28ec63ae3b22caf10fc
Shayan Ahmad <v-shahmad@hotels.com>
2020-10-10 02:06:37 +0200

    ShayanFlutterLearning: Learnt about stateless/ful and inputs/interaction.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MaterialApp(
+      home: BallPage(),
+    ));
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[300],
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Magic8Ball(),
+    );
+    ;
+  }
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballNumber = 1;
+
+  void changeBallAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          changeBallAnswer();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
+  }
+}
