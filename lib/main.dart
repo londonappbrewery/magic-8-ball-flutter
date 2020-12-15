c875d8a4e2ac71fab0173f004c50c65b8e1d2794
Cooper Nichols <coopnich92@gmail.com>
2020-12-15 15:37:53 -0600

    8 ball complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Center(
+          child: Text('Ask Me Anything'),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+
+  void randomizer() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            randomizer();
+          },
+          child: Image.asset('images/ball$answer.png'),
+        ),
+      ),
+    );
+  }
+}
