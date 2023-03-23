148fe16506c24d6129f422ef9fbb68b0475426e7
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-23 12:52:01 +0530

    App Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text(
+              'Ask Me Anything',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: Magic8Ball(),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  State<Magic8Ball> createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballNumber = 1;
+  void newNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            newNumber();
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
